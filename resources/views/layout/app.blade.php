61121a2d50facce8f027572fc6e653a96fde24ff
BriolVictoria <victoria.briol@student.hepl.be>
2025-10-22 14:11:18 +0200

    Added side menu with components

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,42 +1,37 @@
-<!doctype html>
-<html lang="{!! App::getLocale(); !!}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
-</head>
-<body  class="flex min-h-screen bg-gray-50">
-<aside class="fixed top-0 left-0 h-screen w-64 bg-white shadow-2xl flex flex-col justify-between">
+<aside class="fixed top-0 left-0 h-screen pr-15 shadow-2xl flex flex-col justify-between rounded-2xl">
+
     <div class="p-6 border-b border-gray-200">
-        <h1 class="text-2xl font-bold text-blue-500">Menu</h1>
+        <h1 class="text-xl font-bold text-blue-500">
+            Bonjour,
+            <span class="block text-2xl text-blue-300">{!! auth()->user()->name !!}</span>
+        </h1>
     </div>
 
     <nav class="p-6">
-        <ul class="space-y-3">
-            <li>
-                <a href="{{ route('jiris.index')}}" class="flex items-center px-4 py-2 rounded-lg hover:bg-blue-100 hover:text-blue-500 transition">
+        <ul>
+            <li class="mb-5">
+                <a href="{{ route('jiris.index')}}" class="text-xl flex items-center px-4 py-2 rounded-lg hover:bg-blue-100 hover:text-blue-500 transition">
                   Mes jiris
                 </a>
             </li>
 
-            <li>
-                <a href="{{ route('contacts.index')}}" class="flex items-center px-4 py-2 rounded-lg hover:bg-blue-100 hover:text-blue-500 transition">
+            <li class="mb-5">
+                <a href="{{ route('contacts.index')}}" class="text-xl flex items-center px-4 py-2 rounded-lg hover:bg-blue-100 hover:text-blue-500 transition">
                     Mes contacts
                 </a>
             </li>
 
-            <li>
-                <a href="{{ route('projects.index')}}" class="flex items-center px-4 py-2 rounded-lg hover:bg-blue-100 hover:text-blue-500 transition">
+            <li class="mb-5">
+                <a href="{{ route('projects.index')}}" class="text-xl flex items-center px-4 py-2 rounded-lg hover:bg-blue-100 hover:text-blue-500 transition">
                     Mes projets
                 </a>
             </li>
         </ul>
     </nav>
     <div class="p-6 border-t border-gray-200">
-        <a href="{{ route('logout')}}" class="flex items-center text-blue-500 hover:text-blue-700 transition">
-            Déconnexion
-        </a>
+        <form action="{{ route('logout') }}" method="post">
+            @csrf
+            <button type="submit" class="flex items-center text-blue-500 hover:text-blue-700 transition">Déconnexion</button>
+        </form>
     </div>
 </aside>
-</body>
-</html>
