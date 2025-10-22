36115d437746b0d2b4551fa3b73de6e8288faf30
BriolVictoria <victoria.briol@student.hepl.be>
2025-10-22 15:11:11 +0200

    Added one component

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -33,11 +33,11 @@
                 <a class="text-xs ml-3 text-blue-500" href="#">{{ __('login.forgotten_password') }}</a>
             @endcomponent
 
-            @component('components.form.buttons.button', ['class' => 'bg-blue-500 text-white p-2 rounded-sm mt-5 w-1/1 hover:bg-sky-700', 'text' => 'Identifiez-vous'])
+            @component('components.form.buttons.button', ['class' => 'bg-blue-500 text-white p-2 rounded-sm mt-5 w-1/1 hover:bg-sky-700', 'text' => 'Créez un compte'])
             @endcomponent
 
 
-            @component('components.form.fields.link', ['text' => 'Pas encore de compte ? ', 'href' => route('register.store'),'text_link' => 'Créer un compte'])
+            @component('components.form.fields.link', ['text' => 'Pas encore de compte ? ', 'href' => route('login.store'),'text_link' => 'Se connecter'])
             @endcomponent
 
         </fieldset>
