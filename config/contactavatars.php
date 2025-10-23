7af05ff96919694d97f0bca6c48857405df07b0e
BriolVictoria <victoria.briol@student.hepl.be>
2025-10-23 19:29:52 +0200

    Added the avatar sender

---
+++ config/contactavatars.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'image_type' => 'jpg',
+    'original_path' => 'images/contacts/originals',
+    'jpeg_compression' => 80,
+    'variant_path_pattern' => 'images/contacts/variants/%sx%s',
+    'sizes' => [
+        '300' => [
+            'width' => '300',
+            'height' => '300',
+        ],
+
+        '600' => [
+
+            'width' => '600',
+            'height' => '600',
+
+        ],
+
+        '900' => [
+
+            'width' => '900',
+            'height' => '900',
+
+
+        ],
+    ]
+];
