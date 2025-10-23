eff9253a1a42eaf56ede552e5bb4996148c1e26e
BriolVictoria <victoria.briol@student.hepl.be>
2025-10-23 19:28:37 +0200

    Added column

--- database/migrations/2025_09_19_091708_create_projects_table.php
+++ database/migrations/2025_09_19_091708_create_projects_table.php
@@ -10,6 +10,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
             $table->string('name');
             $table->timestamps();
         });
